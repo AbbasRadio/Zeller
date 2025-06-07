2777d070d931ffd3944f21657a31954ec5d68ccf
AbbasRadio <abbasradio1234.ar@gmail.com>
2025-06-07 11:30:47 +0530

    changes made for icons

--- App/src/navigation/MainNavigation.tsx
+++ App/src/navigation/MainNavigation.tsx
@@ -3,10 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Users from '../screens/Users';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialIcons'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import SearchUser from '../screens/SearchUser';
-import { Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,10 +12,18 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={() => ({
-        tabBarIcon: ({ focused, color, size }) => {
-            return <Text style={{color: focused ? '#007bff' : 'gray', fontSize: size / 1.5}}>👤</Text>;
-          },
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName:string='';
+
+          if (route.name === 'AllCustomers') {
+            iconName = focused ? 'person' : 'person-outline';
+          } else if (route.name === 'SearchCustomer') {
+            iconName = focused ? 'search' : 'search-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
         tabBarActiveTintColor: '#007bff',
         tabBarInactiveTintColor: 'gray',
       })}>
